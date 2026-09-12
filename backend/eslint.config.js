10368f7c8dd91dc46679ba1738f5b9f1998ef538
Kartik Kumar <2k25cse2513784@gmail.com>
2026-09-12 14:30:56 +0530

    Foundation completed

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,12 +1,14 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
+export default [
   { ignores: ['dist', 'node_modules'] },
+  js.configs.recommended,
+  ...tseslint.configs['flat/recommended'],
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['src/**/*.ts'],
-    languageOptions: { globals: globals.node },
+    languageOptions: { globals: globals.node, parser: tsParser },
   },
-);
+];
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,18 +2,20 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
+export default [
   { ignores: ['dist', 'node_modules'] },
+  js.configs.recommended,
+  ...tseslint.configs['flat/recommended'],
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: globals.browser, parser: tsParser },
     plugins: { 'react-hooks': reactHooks, 'react-refresh': reactRefresh },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-);
+];
